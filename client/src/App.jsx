648d104d448fb9842c93b4c71ad6926f45e24195
peerawet <peerawet1996@gmail.com>
2023-12-26 16:08:44 +0700

    ex-3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,17 @@ import axios from "axios";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const getProducts = async () => {
-    const result = await axios.get("http://localhost:4001/products");
-    console.log(result.data.data);
-    setProducts(result.data.data);
+    try {
+      const result = await axios.get("http://localhost:4001/products");
+      setLoading(false);
+
+      setProducts(result.data.data);
+    } catch (error) {
+      alert("Fetching error");
+    }
   };
 
   useEffect(() => {
@@ -17,7 +24,8 @@ function App() {
   const deleteProducts = async (id) => {
     await axios
       .delete(`http://localhost:4001/products/${id}`)
-      .then(getProducts);
+      .then(getProducts)
+      .catch(() => alert("error"));
   };
 
   return (
@@ -25,36 +33,40 @@ function App() {
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
-      <div className="product-list">
-        {products.map((data, index) => {
-          return (
-            <div className="product" key={index}>
-              <div className="product-preview">
-                <img
-                  src={data.image}
-                  alt="some product"
-                  width="350"
-                  height="350"
-                />
-              </div>
-              <div className="product-detail">
-                <h1>Product name: {data.name}</h1>
-                <h2>Product price: {data.price} Baht</h2>
-                <p>Product description: {data.description}</p>
-              </div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="product-list">
+          {products.map((data, index) => {
+            return (
+              <div className="product" key={index}>
+                <div className="product-preview">
+                  <img
+                    src={data.image}
+                    alt="some product"
+                    width="350"
+                    height="350"
+                  />
+                </div>
+                <div className="product-detail">
+                  <h1>Product name: {data.name}</h1>
+                  <h2>Product price: {data.price} Baht</h2>
+                  <p>Product description: {data.description}</p>
+                </div>
 
-              <button
-                className="delete-button"
-                onClick={() => {
-                  deleteProducts(data.id);
-                }}
-              >
-                x
-              </button>
-            </div>
-          );
-        })}
-      </div>
+                <button
+                  className="delete-button"
+                  onClick={() => {
+                    deleteProducts(data.id);
+                  }}
+                >
+                  x
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
